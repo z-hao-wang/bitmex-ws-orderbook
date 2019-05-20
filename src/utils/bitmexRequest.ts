29aac96fcd145fe45d5f4f067ebe3df7b092e774
wanghao <whateverhow@gmail.com>
2019-05-20 00:17:01 -0700

    [master] 	modified:   package-lock.json

--- src/utils/bitmexRequest.ts
+++ src/utils/bitmexRequest.ts
@@ -1,16 +1,7 @@
-import * as request from 'request';
-import { requestRetry } from './retry';
 import * as _ from 'lodash';
 import { sortOrderBooks } from './parsingUtils';
+import { BitmexRequest } from 'bitmex-request';
 export type Method = 'POST' | 'GET' | 'PUT' | 'DELETE';
-
-const API_VERSION = '/api/v1';
-const URL_TESTNET = 'https://testnet.bitmex.com' + API_VERSION;
-const URL_PROD = 'https://www.bitmex.com' + API_VERSION;
-
-export function getUrl(testnet: boolean) {
-  return testnet ? URL_TESTNET : URL_PROD;
-}
 
 export async function bitmexRequest<T = any>(
   method: Method,
@@ -19,45 +10,10 @@ export async function bitmexRequest<T = any>(
   retryTimes: number,
   testnet: boolean,
 ): Promise<T> {
-  const postBody = JSON.stringify(data);
-  let headers: any = {
-    'content-type': 'application/json',
-    Accept: 'application/json',
-    'X-Requested-With': 'XMLHttpRequest',
-  };
-  const fullUrl = getUrl(testnet) + path;
-  const requestOptions = {
-    headers: headers,
-    url: fullUrl,
-    method,
-    body: postBody,
-  };
-  const requestFunc = async () => {
-    return new Promise<T>((resolve, reject) => {
-      request(requestOptions, (error, response, body) => {
-        if (error) {
-          return reject(error);
-        }
-        if (!body) {
-          const errMsg = `empty result, ${method} ${path} ${data}`;
-          return reject(errMsg);
-        }
-        try {
-          const parsedBody = JSON.parse(body);
-          resolve(parsedBody);
-        } catch (e) {
-          const errMsg = `parse body json failed, ${method} ${path} ${body}`;
-          reject(errMsg);
-        }
-      });
-    });
-  };
-  return requestRetry(requestFunc, {
-    maxRetryTimes: retryTimes,
-    successCondition: (res: any) => {
-      return res && !res.error;
-    },
+  const bitmexRequest = new BitmexRequest({
+    testnet,
   });
+  return bitmexRequest.request(method, path, data, false, retryTimes);
 }
 
 async function getOrderBookHttp(pair: string, testnet: boolean): Promise<Bitmex.BitmexOrderBooks> {
