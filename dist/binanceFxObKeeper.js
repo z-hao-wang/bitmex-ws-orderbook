9aa2594574c3673e196df1b679f81eb6869638da
wanghao <whateverhow@gmail.com>
2020-02-20 17:54:56 -0800

    [master] 	modified:   dist/binanceFxObKeeper.js

--- dist/binanceFxObKeeper.js
+++ dist/binanceFxObKeeper.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const genericObKeeper_1 = require("./genericObKeeper");
 const _ = require("lodash");
-const autoParseFloat = (v) => _.isString(v) ? parseFloat(v) : v;
+const autoParseFloat = (v) => (_.isString(v) ? parseFloat(v) : v);
 function binanceObToStandardOb(v) {
     return { r: autoParseFloat(v[0]), a: autoParseFloat(v[1]) };
 }
@@ -13,7 +13,11 @@ class BinanceFxObKeeper extends genericObKeeper_1.GenericObKeeper {
             const res = _.isString(msg) ? JSON.parse(msg) : msg;
             const { data } = res;
             if (data.e === 'depthUpdate') {
-                this.onReceiveOb({ pair: data.s.toLowerCase(), bids: _.map(data.b, binanceObToStandardOb), asks: _.map(data.a, binanceObToStandardOb) });
+                this.onReceiveOb({
+                    pair: data.s.toLowerCase(),
+                    bids: _.map(data.b, binanceObToStandardOb),
+                    asks: _.map(data.a, binanceObToStandardOb),
+                });
             }
         }
         catch (e) {
